0dcf86a5f7a9023f724ee7b79e0e5f5c0a6849a7
Matt Young <mattyoung305@gmail.com>
2021-12-19 18:49:19 +0200

    Initial commit.
    Webpack runner currently not working.

---
+++ babel.config.js
@@ -0,0 +1,27 @@
+module.exports = {
+  presets: [
+    "@babel/preset-env",
+    "@babel/preset-typescript",
+    "@babel/preset-react",
+  ],
+  plugins: [
+    "@babel/plugin-transform-runtime",
+    "@babel/plugin-syntax-dynamic-import",
+    "@babel/plugin-proposal-class-properties",
+  ],
+  env: {
+    production: {
+      only: ["src"],
+      plugins: [
+        [
+          "transform-react-remove-prop-types",
+          {
+            removeImport: true,
+          },
+        ],
+        "@babel/plugin-transform-react-inline-elements",
+        "@babel/plugin-transform-react-constant-elements",
+      ],
+    },
+  },
+};
--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,43 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.scss";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  render(): React.ReactNode {
+    return (
+      <div className="App">
+        <nav>
+          <div className="page-links">
+            <a className="nav-link" href="">
+              Home
+            </a>
+            <a className="nav-link" href="">
+              Projects
+            </a>
+            <a className="nav-link" href="">
+              About
+            </a>
+          </div>
+          <div className="socials">
+            <button id="github" className="logo"></button>
+            <a
+              className="nav-link"
+              href="https://github.com/mattgoespro"
+              target="tab"
+            >
+              <img src="./assets/media/images/github-logo.png" />
+            </a>
+            <a
+              className="nav-link"
+              href="https://www.linkedin.com/in/matt-young-691b48189/"
+              target="tab"
+            >
+              <img src="./assets/media/images/linkedin-logo.png" />
+            </a>
+          </div>
+        </nav>
+      </div>
+    );
+  }
 }
 
 export default App;
