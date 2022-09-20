bf8f07a4bd30d158b91d420a05ae1af963a2152e
mattgoespro <mattyoung305@gmail.com>
2022-09-20 18:31:33 +0200

    Package upgrades

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,8 @@
-import { render } from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import App from './app/App';
 
-render(<App />, document.getElementById('root'));
+const root = createRoot(document.getElementById('root')); // createRoot(container!) if you use TypeScript
+root.render(<App />);
 
 window.onbeforeunload = function () {
   window.scrollTo(0, 0);
