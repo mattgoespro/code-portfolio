2848924d214fcd1189d12f99f2466cd89dcc671c
mattgoespro <mattyoung305@gmail.com>
2022-09-24 14:44:12 +0200

    Checkpoint

--- src/app/components/projects/project-list/project/Project.tsx
+++ src/app/components/projects/project-list/project/Project.tsx
@@ -1,9 +1,7 @@
-import { GitHub } from '@mui/icons-material';
 import { ApiRepositoryResponseDTO } from '@shared/services/shared.dto';
 import './Project.scss';
 import { Link } from 'react-router-dom';
-import { Link as LinkButton } from '@mui/material';
-import Button from '@mui/material/Button';
+import { Button, Link as ExternalLink } from '@mui/material';
 
 interface ProjectProps {
   repo: ApiRepositoryResponseDTO;
@@ -17,23 +15,18 @@ export default function Project(props: ProjectProps) {
     <div className="project-card">
       <div className="project-card-title">
         <div className="project-name">{repo.friendlyName || repo.repositoryName}</div>
-        <div className="project-actions">
-          <LinkButton
-            className="project-view-github"
-            href={repo.link}
-            target="tab"
-            underline="none"
-          >
-            <Button>See Repo</Button>
-          </LinkButton>
-          <Link to={`/projects/${repo.repositoryName}`} className="link-github">
-            <Button className="btn-view-details" disableRipple>
-              More Details
-            </Button>
-          </Link>
+        <div className="project-nav-links">
+          <ExternalLink href={repo.link} target="tab" underline="none">
+            <Button className="btn-project-nav-link">GitHub</Button>
+          </ExternalLink>
+
+          <Button className="btn-project-nav-link">
+            <Link className="lnk-view-details" to={`/projects/${repo.repositoryName}`}>
+              Details
+            </Link>
+          </Button>
         </div>
       </div>
-      <div className="divider title-divider"></div>
       <div className="description-wrapper">
         <div className="title-description">Description</div>
         <div className="description">{repo.description || <i>Not available.</i>}</div>
